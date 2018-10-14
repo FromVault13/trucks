caf4478f847a746ca3772a2f1d8224110da8f5fe
Emil <sultanov-emil@mail.ru>
2018-10-14 21:37:28 +0300

    version 4

--- application/settings.py
+++ application/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_HOST = 'https://d4663kmspf1sqa.cloudfront.net' if not DEBUG else ''
-STATIC_URL = STATIC_HOST + '/static/
+STATIC_URL = STATIC_HOST + '/static/'
 
 cwd = os.getcwd()
 print("--- CWD ---\n", cwd, "\n---\n")
